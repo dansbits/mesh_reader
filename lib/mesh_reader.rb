eb669c7e987e1eeac4076221a709c03349c06fbc
Daniel Carpenter <daniel.carpenter01@gmail.com>
2014-06-28 15:12:56 -0700

    Refactor to require necessary files in mesh_reader.rb

--- lib/mesh_reader.rb
+++ lib/mesh_reader.rb
@@ -1,6 +1,17 @@
+require 'descriptor'
+require 'descriptor_list'
+require 'concept'
+require 'term'
+
 module MeshReader
   extend self
 
+  # Attempts to open the specified XML file and returns a DescriptorList
+  # object for retrieval of MeSH data.do
+  #
+  # @param file_path [String] The path to the MeSH xml file
+  # @return [DescriptorList] A DescriptorList containing the MeSH terms in the
+  # specified file
   def parse file_path
     xml = File.open(file_path)
 
